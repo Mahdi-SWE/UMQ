53a69fd9d8b9ff9f7e2b8048aae4ed6bf87fb63b
Mahdi-SWE <mahoodi14.boshanb@gmail.com>
2024-10-09 23:06:05 +0300

    about and contact pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,154 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    About
-  </h1>
+  <div class="relative flex size-full min-h-screen flex-col bg-[#f8f9fc] group/design-root overflow-x-hidden"
+    style='font-family: "Public Sans", "Noto Sans", sans-serif;'>
+    <div class="layout-container flex h-full grow flex-col">
+      <div class="px-40 flex flex-1 justify-center py-5">
+        <div class="layout-content-container flex flex-col max-w-[960px] flex-1">
+          <div class="@container">
+            <div class="flex flex-col gap-6 px-4 py-10 @[480px]:gap-8 @[864px]:flex-row">
+              <div
+                class="w-full bg-center bg-no-repeat aspect-video bg-cover rounded-xl @[480px]:h-auto @[480px]:min-w-[400px] @[864px]:w-full"
+                style='background-image: url("https://cdn.usegalileo.ai/sdxl10/ea468b77-f07c-4328-bd3f-eb58e2218312.png");'>
+              </div>
+              <div class="flex flex-col gap-6 @[480px]:min-w-[400px] @[480px]:gap-8 @[864px]:justify-center">
+                <div class="flex flex-col gap-2 text-left">
+                  <h1
+                    class="text-[#0e101b] text-4xl font-black leading-tight tracking-[-0.033em] @[480px]:text-5xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em]">
+                    Deep clean, every time
+                  </h1>
+                  <h2
+                    class="text-[#0e101b] text-sm font-normal leading-normal @[480px]:text-base @[480px]:font-normal @[480px]:leading-normal">
+                    At UMQ, we're committed to using the safest certified materials for our clients. We're dedicated
+                    to providing deep cleaning services at affordable prices. Our
+                    mission is to provide the best possible service to our clients, and we're always looking for ways
+                    to improve.
+                  </h2>
+                </div>
+                <label class="flex flex-col min-w-40 h-14 w-full max-w-[480px] @[480px]:h-16">
+                  <div class="flex w-full flex-1 items-stretch rounded-xl h-full">
+                    <div
+                      class="text-[#4e5997] flex border-none bg-[#e7e9f3] items-center justify-center pl-4 rounded-l-xl border-r-0"
+                      data-icon="MagnifyingGlass" data-size="20px" data-weight="regular">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor"
+                        viewBox="0 0 256 256">
+                        <path
+                          d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z">
+                        </path>
+                      </svg>
+                    </div>
+                    <input placeholder="Enter your email address"
+                      class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#0e101b] focus:outline-0 focus:ring-0 border-none bg-[#e7e9f3] focus:border-none h-full placeholder:text-[#4e5997] px-4 rounded-r-none border-r-0 pr-2 rounded-l-none border-l-0 pl-2 text-sm font-normal leading-normal @[480px]:text-base @[480px]:font-normal @[480px]:leading-normal"
+                      value="" />
+                    <div
+                      class="flex items-center justify-center rounded-r-xl border-l-0 border-none bg-[#e7e9f3] pr-2">
+                      <button
+                        class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 @[480px]:h-12 @[480px]:px-5 bg-[#556bec] text-[#f8f9fc] text-sm font-bold leading-normal tracking-[0.015em] @[480px]:text-base @[480px]:font-bold @[480px]:leading-normal @[480px]:tracking-[0.015em]">
+                        <span class="truncate">Sign Up</span>
+                      </button>
+                    </div>
+                  </div>
+                </label>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col gap-10 px-4 py-10 @container">
+            <h1
+              class="text-[#0e101b] tracking-light text-[32px] font-bold leading-tight @[480px]:text-4xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em] max-w-[720px]">
+              Why choose us
+            </h1>
+          </div>
+          <h2 class="text-[#0e101b] text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 pb-3 pt-5">Our
+            History</h2>
+          <div class="grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3 p-4">
+            <div class="flex flex-1 gap-3 rounded-lg border border-[#d0d3e7] bg-[#f8f9fc] p-4 flex-col">
+              <div class="bg-center bg-no-repeat aspect-square bg-cover rounded-full w-10 shrink-0"
+                style='background-image: url("https://cdn.usegalileo.ai/sdxl10/ca743263-3f16-4969-95c3-2382abb8c22f.png");'>
+              </div>
+              <div class="flex flex-col gap-1">
+                <h2 class="text-[#0e101b] text-base font-bold leading-tight">2010</h2>
+                <p class="text-[#4e5997] text-sm font-normal leading-normal">Founded</p>
+              </div>
+            </div>
+            <div class="flex flex-1 gap-3 rounded-lg border border-[#d0d3e7] bg-[#f8f9fc] p-4 flex-col">
+              <div class="bg-center bg-no-repeat aspect-square bg-cover rounded-full w-10 shrink-0"
+                style='background-image: url("https://cdn.usegalileo.ai/sdxl10/8b50cbb0-8282-4651-8f8c-50bd6177e7b6.png");'>
+              </div>
+              <div class="flex flex-col gap-1">
+                <h2 class="text-[#0e101b] text-base font-bold leading-tight">2012</h2>
+                <p class="text-[#4e5997] text-sm font-normal leading-normal">Awarded Best Cleaning Service</p>
+              </div>
+            </div>
+            <div class="flex flex-1 gap-3 rounded-lg border border-[#d0d3e7] bg-[#f8f9fc] p-4 flex-col">
+              <div class="bg-center bg-no-repeat aspect-square bg-cover rounded-full w-10 shrink-0"
+                style='background-image: url("https://cdn.usegalileo.ai/stability/65a7a091-fb05-491d-8038-9cb115f366bf.png");'>
+              </div>
+              <div class="flex flex-col gap-1">
+                <h2 class="text-[#0e101b] text-base font-bold leading-tight">2014</h2>
+                <p class="text-[#4e5997] text-sm font-normal leading-normal">Expanded to More Locations</p>
+              </div>
+            </div>
+            <div class="flex flex-1 gap-3 rounded-lg border border-[#d0d3e7] bg-[#f8f9fc] p-4 flex-col">
+              <div class="bg-center bg-no-repeat aspect-square bg-cover rounded-full w-10 shrink-0"
+                style='background-image: url("https://cdn.usegalileo.ai/sdxl10/89d974f2-b3b4-4795-bf27-bcb3796d5314.png");'>
+              </div>
+              <div class="flex flex-col gap-1">
+                <h2 class="text-[#0e101b] text-base font-bold leading-tight">2016</h2>
+                <p class="text-[#4e5997] text-sm font-normal leading-normal">Launched UMQ Eco-Friendly Line</p>
+              </div>
+            </div>
+            <div class="flex flex-1 gap-3 rounded-lg border border-[#d0d3e7] bg-[#f8f9fc] p-4 flex-col">
+              <div class="bg-center bg-no-repeat aspect-square bg-cover rounded-full w-10 shrink-0"
+                style='background-image: url("https://cdn.usegalileo.ai/sdxl10/f6ca8e3e-eb11-4778-957b-2d1e42960008.png");'>
+              </div>
+              <div class="flex flex-col gap-1">
+                <h2 class="text-[#0e101b] text-base font-bold leading-tight">2018</h2>
+                <p class="text-[#4e5997] text-sm font-normal leading-normal">Reached 10,000 Happy Clients</p>
+              </div>
+            </div>
+            <div class="flex flex-1 gap-3 rounded-lg border border-[#d0d3e7] bg-[#f8f9fc] p-4 flex-col">
+              <div class="bg-center bg-no-repeat aspect-square bg-cover rounded-full w-10 shrink-0"
+                style='background-image: url("https://cdn.usegalileo.ai/stability/01822f55-8c2f-4213-a535-9c5055a86955.png");'>
+              </div>
+              <div class="flex flex-col gap-1">
+                <h2 class="text-[#0e101b] text-base font-bold leading-tight">2020</h2>
+                <p class="text-[#4e5997] text-sm font-normal leading-normal">Launched UMQ Cleaning Academy</p>
+              </div>
+            </div>
+          </div>
+          <h2 class="text-[#0e101b] text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 pb-3 pt-5">Our Team
+          </h2>
+          <div class="grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3 p-4">
+            <div class="flex flex-col gap-3">
+              <div class="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-xl"
+                style='background-image: url("https://cdn.usegalileo.ai/sdxl10/90d72973-31dd-4027-bfd6-7247440fa986.png");'>
+              </div>
+            </div>
+            <div class="flex flex-col gap-3">
+              <div class="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-xl"
+                style='background-image: url("https://cdn.usegalileo.ai/sdxl10/264339a8-0c9a-493b-9eac-4981dc6ac98c.png");'>
+              </div>
+            </div>
+            <div class="flex flex-col gap-3">
+              <div class="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-xl"
+                style='background-image: url("https://cdn.usegalileo.ai/sdxl10/a5e946db-767b-48a3-914b-49328bb6593d.png");'>
+              </div>
+            </div>
+            <div class="flex flex-col gap-3">
+              <div class="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-xl"
+                style='background-image: url("https://cdn.usegalileo.ai/sdxl10/5836a34b-cfe0-4048-9640-b255d632cc18.png");'>
+              </div>
+            </div>
+            <div class="flex flex-col gap-3">
+              <div class="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-xl"
+                style='background-image: url("https://cdn.usegalileo.ai/sdxl10/5078fb87-d069-4762-a348-3124881d2c05.png");'>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-
 </script>
---
+++ pages/contact.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="px-40 flex flex-1 justify-center py-5">
+        <div class="layout-content-container flex flex-col max-w-[960px] flex-1">
+            <div class="flex flex-wrap justify-between gap-3 p-4">
+                <p class="text-[#0e101b] tracking-light text-[32px] font-bold leading-tight min-w-72">Contact us</p>
+            </div>
+            <h3 class="text-[#0e101b] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">Customer
+                Service</h3>
+            <div class="flex items-center gap-4 bg-[#f8f9fc] px-4 min-h-[72px] py-2">
+                <div class="text-[#0e101b] flex items-center justify-center rounded-lg bg-[#e7e9f3] shrink-0 size-12"
+                    data-icon="Clock" data-size="24px" data-weight="regular">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor"
+                        viewBox="0 0 256 256">
+                        <path
+                            d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm64-88a8,8,0,0,1-8,8H128a8,8,0,0,1-8-8V72a8,8,0,0,1,16,0v48h48A8,8,0,0,1,192,128Z">
+                        </path>
+                    </svg>
+                </div>
+                <div class="flex flex-col justify-center">
+                    <p class="text-[#0e101b] text-base font-medium leading-normal line-clamp-1">Business Hours</p>
+                    <p class="text-[#4e5997] text-sm font-normal leading-normal line-clamp-2">Mon-Fri: 9am-6pm</p>
+                </div>
+            </div>
+            <div class="flex items-center gap-4 bg-[#f8f9fc] px-4 min-h-14">
+                <div class="text-[#0e101b] flex items-center justify-center rounded-lg bg-[#e7e9f3] shrink-0 size-10"
+                    data-icon="ShieldCheck" data-size="24px" data-weight="regular">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor"
+                        viewBox="0 0 256 256">
+                        <path
+                            d="M208,40H48A16,16,0,0,0,32,56v58.78c0,89.61,75.82,119.34,91,124.39a15.53,15.53,0,0,0,10,0c15.2-5.05,91-34.78,91-124.39V56A16,16,0,0,0,208,40Zm0,74.79c0,78.42-66.35,104.62-80,109.18-13.53-4.51-80-30.69-80-109.18V56H208ZM82.34,141.66a8,8,0,0,1,11.32-11.32L112,148.68l50.34-50.34a8,8,0,0,1,11.32,11.32l-56,56a8,8,0,0,1-11.32,0Z">
+                        </path>
+                    </svg>
+                </div>
+                <p class="text-[#0e101b] text-base font-normal leading-normal flex-1 truncate">Emergency Service
+                    Available</p>
+            </div>
+            <h3 class="text-[#0e101b] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">Contact
+                Information</h3>
+            <div class="flex items-center gap-4 bg-[#f8f9fc] px-4 min-h-14">
+                <div class="text-[#0e101b] flex items-center justify-center rounded-lg bg-[#e7e9f3] shrink-0 size-10"
+                    data-icon="Phone" data-size="24px" data-weight="regular">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor"
+                        viewBox="0 0 256 256">
+                        <path
+                            d="M222.37,158.46l-47.11-21.11-.13-.06a16,16,0,0,0-15.17,1.4,8.12,8.12,0,0,0-.75.56L134.87,160c-15.42-7.49-31.34-23.29-38.83-38.51l20.78-24.71c.2-.25.39-.5.57-.77a16,16,0,0,0,1.32-15.06l0-.12L97.54,33.64a16,16,0,0,0-16.62-9.52A56.26,56.26,0,0,0,32,80c0,79.4,64.6,144,144,144a56.26,56.26,0,0,0,55.88-48.92A16,16,0,0,0,222.37,158.46ZM176,208A128.14,128.14,0,0,1,48,80,40.2,40.2,0,0,1,82.87,40a.61.61,0,0,0,0,.12l21,47L83.2,111.86a6.13,6.13,0,0,0-.57.77,16,16,0,0,0-1,15.7c9.06,18.53,27.73,37.06,46.46,46.11a16,16,0,0,0,15.75-1.14,8.44,8.44,0,0,0,.74-.56L168.89,152l47,21.05h0s.08,0,.11,0A40.21,40.21,0,0,1,176,208Z">
+                        </path>
+                    </svg>
+                </div>
+                <p class="text-[#0e101b] text-base font-normal leading-normal flex-1 truncate">123-456-7890</p>
+            </div>
+            <div class="flex items-center gap-4 bg-[#f8f9fc] px-4 min-h-14">
+                <div class="text-[#0e101b] flex items-center justify-center rounded-lg bg-[#e7e9f3] shrink-0 size-10"
+                    data-icon="EnvelopeOpen" data-size="24px" data-weight="regular">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor"
+                        viewBox="0 0 256 256">
+                        <path
+                            d="M228.44,89.34l-96-64a8,8,0,0,0-8.88,0l-96,64A8,8,0,0,0,24,96V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V96A8,8,0,0,0,228.44,89.34ZM96.72,152,40,192V111.53Zm16.37,8h29.82l56.63,40H56.46Zm46.19-8L216,111.53V192ZM128,41.61l81.91,54.61-67,47.78H113.11l-67-47.78Z">
+                        </path>
+                    </svg>
+                </div>
+                <p class="text-[#0e101b] text-base font-normal leading-normal flex-1 truncate">info@umqcleaning.com</p>
+            </div>
+            <h3 class="text-[#0e101b] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">Address</h3>
+            <div class="flex items-center gap-4 bg-[#f8f9fc] px-4 min-h-[72px] py-2">
+                <div class="text-[#0e101b] flex items-center justify-center rounded-lg bg-[#e7e9f3] shrink-0 size-12"
+                    data-icon="MapPin" data-size="24px" data-weight="regular">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor"
+                        viewBox="0 0 256 256">
+                        <path
+                            d="M128,64a40,40,0,1,0,40,40A40,40,0,0,0,128,64Zm0,64a24,24,0,1,1,24-24A24,24,0,0,1,128,128Zm0-112a88.1,88.1,0,0,0-88,88c0,31.4,14.51,64.68,42,96.25a254.19,254.19,0,0,0,41.45,38.3,8,8,0,0,0,9.18,0A254.19,254.19,0,0,0,174,200.25c27.45-31.57,42-64.85,42-96.25A88.1,88.1,0,0,0,128,16Zm0,206c-16.53-13-72-60.75-72-118a72,72,0,0,1,144,0C200,161.23,144.53,209,128,222Z">
+                        </path>
+                    </svg>
+                </div>
+                <div class="flex flex-col justify-center">
+                    <p class="text-[#0e101b] text-base font-medium leading-normal line-clamp-1">123 Main St, San
+                        Francisco, CA 94111</p>
+                    <p class="text-[#4e5997] text-sm font-normal leading-normal line-clamp-2">123 Main St, San
+                        Francisco, CA 94111</p>
+                </div>
+            </div>
+            <h3 class="text-[#0e101b] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">Get In Touch
+            </h3>
+            <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+                <label class="flex flex-col min-w-40 flex-1">
+                    <input placeholder="Name"
+                        class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#0e101b] focus:outline-0 focus:ring-0 border border-[#d0d3e7] bg-[#f8f9fc] focus:border-[#d0d3e7] h-14 placeholder:text-[#4e5997] p-[15px] text-base font-normal leading-normal"
+                        value="" />
+                </label>
+            </div>
+            <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+                <label class="flex flex-col min-w-40 flex-1">
+                    <input placeholder="Email"
+                        class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#0e101b] focus:outline-0 focus:ring-0 border border-[#d0d3e7] bg-[#f8f9fc] focus:border-[#d0d3e7] h-14 placeholder:text-[#4e5997] p-[15px] text-base font-normal leading-normal"
+                        value="" />
+                </label>
+            </div>
+            <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+                <label class="flex flex-col min-w-40 flex-1">
+                    <input placeholder="Phone"
+                        class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#0e101b] focus:outline-0 focus:ring-0 border border-[#d0d3e7] bg-[#f8f9fc] focus:border-[#d0d3e7] h-14 placeholder:text-[#4e5997] p-[15px] text-base font-normal leading-normal"
+                        value="" />
+                </label>
+            </div>
+            <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+                <label class="flex flex-col min-w-40 flex-1">
+                    <textarea placeholder="Message"
+                        class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#0e101b] focus:outline-0 focus:ring-0 border border-[#d0d3e7] bg-[#f8f9fc] focus:border-[#d0d3e7] min-h-36 placeholder:text-[#4e5997] p-[15px] text-base font-normal leading-normal"></textarea>
+                </label>
+            </div>
+            <div class="flex px-4 py-3 justify-end">
+                <button
+                    class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#556bec] text-[#f8f9fc] text-sm font-bold leading-normal tracking-[0.015em]">
+                    <span class="truncate">Send</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
